56a4cb73bf1be609f3e0eeacd0209cbac977965c
Paul Keith <javelinanddart@gmail.com>
2015-11-09 07:43:40 -0800

    jfltecri: Initital Bringup to M
    * Based on jflteusc
    
    Change-Id: Ic3e2c5641e476f6328bc5a16caf6aab79db04c86
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,13 +21,10 @@
 #
 
 # inherit from common JF
--include device/samsung/jf-common/BoardConfigCommon.mk
+include device/samsung/jf-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/jflter970/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := jfltecri
-
-# Kernel
-TARGET_KERNEL_CONFIG    := jf_cri_defconfig
+TARGET_OTA_ASSERT_DEVICE := jfltecri,jflte
